390c740f24b60bd851f4d503beda50feca1f57fe
Oladele Joshua <jurshsmith@gmail.com>
2021-07-21 22:10:53 +0100

    just playing around

---
+++ priority_queue/priority_queue.ex
@@ -0,0 +1,23 @@
+defmodule Algo.PriorityQueue do
+  def transform_list_to_max_heap(list) when is_list(list) do
+    list
+    |> Enum.map(&(&1 * 2))
+  end
+
+  def transform_list_to_min_heap do
+  end
+end
+
+# mix helps our when trying to break modules into files
+# Could also use the Code module to Code.require_file an elixir script file (.exs)
+defmodule Algo.PriorityQueue.Consumer do
+  def consume do
+    [1, 2, 3, 4, 5, 5]
+    |> Algo.PriorityQueue.transform_list_to_max_heap
+    |> Enum.each(&(IO.puts(&1)))
+
+    # using the arity will not allow an enum to pass to the function since function params are also enumerable
+  end
+end
+
+Algo.PriorityQueue.Consumer.consume()
