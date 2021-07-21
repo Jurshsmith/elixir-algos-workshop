0fd42b9ba439d54850a0e3f9af1d697e243909fa
Oladele Joshua <jurshsmith@gmail.com>
2021-07-21 13:45:31 +0100

    updates for simple age validator

---
+++ simple_age_validator/simple_age_validator.ex
@@ -0,0 +1,32 @@
+defmodule Algo.SimpleAgeValidator do
+  def get_age_bracket(age_value) when is_number(age_value) do
+    # Case with pattern matching
+    case age_value do
+      x when x >= 0 and x <= 12 -> :kid
+      x when x > 12 and x <= 21 -> :teen
+      x when x > 21 and x <= 60 -> :adult
+      x when x > 60 -> :old
+      _ -> "Not in any age bracket"
+    end
+  end
+end
+
+defmodule Algo.SimpleAgeValidator.Consumer do
+  import Algo.SimpleAgeValidator
+
+  def simulate_age() do
+    # Use lists [] for large iterables
+    # Use tuples {} for small data set that have are related
+    # Use map %{} for map use cases
+
+    agesToSimulate = [12, 20, 40, 23, 50, 21, 4, 23, 13];
+
+    # this arrangement infers that first param is from the top and second param for .each function is what you passed to it
+    agesToSimulate
+    |> Enum.map(&get_age_bracket/1)
+    |> Enum.each(&IO.puts/1)
+  end
+end
+
+
+Algo.SimpleAgeValidator.Consumer.simulate_age();
