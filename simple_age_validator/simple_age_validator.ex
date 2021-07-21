47afe3c360f5d2261810a86ee238475d8c0263e0
Oladele Joshua <jurshsmith@gmail.com>
2021-07-21 14:19:21 +0100

    some try and catch

--- simple_age_validator/simple_age_validator.ex
+++ simple_age_validator/simple_age_validator.ex
@@ -1,6 +1,7 @@
 defmodule Algo.SimpleAgeValidator do
   def get_age_bracket(age_value) when is_number(age_value) do
     # Case with pattern matching
+    # With statement with a use case of nested conditionals. With statement gives a perfect control flow in that situation.
     case age_value do
       x when x >= 0 and x <= 12 -> :kid
       x when x > 12 and x <= 21 -> :teen
@@ -9,6 +10,10 @@ defmodule Algo.SimpleAgeValidator do
       _ -> "Not in any age bracket"
     end
   end
+
+  def get_age_bracket(_) do
+    throw({ :error, "Invalid Type" })
+  end
 end
 
 defmodule Algo.SimpleAgeValidator.Consumer do
@@ -16,17 +21,21 @@ defmodule Algo.SimpleAgeValidator.Consumer do
 
   def simulate_age() do
     # Use lists [] for large iterables
-    # Use tuples {} for small data set that have are related
+    # Use tuples {} for small data set that are related
     # Use map %{} for map use cases
 
-    agesToSimulate = [12, 20, 40, 23, 50, 21, 4, 23, 13];
+    agesToSimulate = ["cool", 12, 20, 40, 23, 50, 21, 4, 23, 13]
 
     # this arrangement infers that first param is from the top and second param for .each function is what you passed to it
-    agesToSimulate
-    |> Enum.map(&get_age_bracket/1)
-    |> Enum.each(&IO.puts/1)
+    try do
+      agesToSimulate
+      |> Enum.map(&get_age_bracket/1)
+      |> Enum.each(&IO.puts/1)
+    catch
+      { :error, msg } -> IO.puts("Error occured #{msg}")
+    end
+
   end
 end
 
-
-Algo.SimpleAgeValidator.Consumer.simulate_age();
+Algo.SimpleAgeValidator.Consumer.simulate_age()
