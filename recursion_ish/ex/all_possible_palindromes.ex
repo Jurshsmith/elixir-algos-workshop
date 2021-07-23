50298ba55c4985aec645659b0568aa7f2e5d6e87
Oladele Joshua <jurshsmith@gmail.com>
2021-07-23 17:15:30 +0100

    done with ex and js versions. Pausing this for now

--- recursion_ish/ex/all_possible_palindromes.ex
+++ recursion_ish/ex/all_possible_palindromes.ex
@@ -1,20 +1,63 @@
-
 defmodule Algo.AllPossiblePalindromes do
-  # When thinking about pattern match, it's like reading a statement in reverse and destructuring
-  def find_all_possiblePalindromes(<<string_to_check::binary>>)  do
-
-   string_to_check
+  # keep temporary state with recursion
+  def find_all_possible_palindromes(<<string_to_check::binary>>) do
+    string_to_check
+    |> find_all_possible_palindromes([], generate_string_length_queue(string_to_check), 1, string_to_check)
   end
 
-  def find_all_possiblePalindromes(_), do: "Invalid type"
+  def find_all_possible_palindromes(_, palindrome_list, [], _, _) do
+    palindrome_list
+  end
+
+  # When thinking about pattern match, it's like reading a statement in reverse and destructuring
+  def find_all_possible_palindromes(<<string_to_check::binary>>, palindrome_list, string_length_queue, count, <<initial_string::binary>>) do
+    # if string is a palindrome, add to the list
+    # no access to shared memory (or process for this task), so use recursion to keep state
+    # tenary sort of
+    # prepending to the list here.
+    palindrome_list = if is_palindrome(string_to_check), do: [ string_to_check | palindrome_list ], else: palindrome_list
+
+    # pattern matching to get head, front of the string length queue
+    [head | tail] = string_length_queue
+
+    if count == head do
+      initial_string = initial_string
+      |> String.slice(1..String.length(initial_string) - 1)
+
+      initial_string
+      |> find_all_possible_palindromes(palindrome_list, tail, 1, initial_string)
+    else
+      count = count + 1
+
+      string_to_check
+      |> String.slice(0..String.length(string_to_check) - 2)
+      |> find_all_possible_palindromes(palindrome_list, string_length_queue, count, initial_string)
+    end
+
+  end
+
+  def find_all_possible_palindromes(_), do: "Invalid type"
+
+  def is_palindrome(<<string_to_check::binary>>) do
+    String.length(string_to_check) > 1 and string_to_check == String.reverse(string_to_check)
+  end
+
+  def generate_string_length_queue(<<string_to_check::binary>>) do
+    max_length = String.length(string_to_check)
+    min_length = 1
+    max_length..min_length
+    |> Enum.to_list
+  end
 end
 
 defmodule Algo.AllPossiblePalindromes.Consumer do
-  def consume do
-    "aabcaacdadsaewawt"
-    |> Algo.AllPossiblePalindromes.find_all_possiblePalindromes
-    |> IO.puts
+  def consume(str) do
+    str
+    |> Algo.AllPossiblePalindromes.find_all_possible_palindromes
+    |> IO.inspect
   end
 end
 
-Algo.AllPossiblePalindromes.Consumer.consume()
+Algo.AllPossiblePalindromes.Consumer.consume("aabcaacdadsaewawtt")
+
+Algo.AllPossiblePalindromes.Consumer.consume("DennisNellEdnaLeonNedraAnitaRolfNoraAliceCarolLeoJaneReedDenaDaleBasilRaePennyLanaDaveDennyLenaIda")
--- recursion_ish/js/all_possible_palindromes.js
+++ recursion_ish/js/all_possible_palindromes.js
@@ -1,6 +1,6 @@
 
 // Would require clean up and validation in real world
-function allPossiblePalindromes(str, state = {}) {
+function allPossiblePalindromes(str, state = { palindromes: [] }) {
   if (str.length > 1) {
     if (!state['initial_string']) {
       state['initial_string'] = str;
@@ -8,25 +8,23 @@ function allPossiblePalindromes(str, state = {}) {
 
     // check if the string is a palindrome and modify state correctly
     if (str === str.split('').reverse().join('')) {
-      state[str] = true;
+      state.palindromes.push(str);
     }
     // javascript passes object by copying their Reference, so we can leverage on this to keep track of the state
     // find all possible palindromes by removing from the end of the string (till we have one letter)
     allPossiblePalindromes(str.slice(0, str.length - 1), state);
 
     // coming out of the stack of the initial string, we want to make the initial string now start from the next letter till the end
-    state['initial_string'] = state['initial_string'].slice(1, state['initial_string'].length);
-
+    state['initial_string'] = state['initial_string'].slice(1, state['initial_string'].length); // Second level of recursion is using the same address in memory instead of copying into a new memory address every single time.
 
     allPossiblePalindromes(state['initial_string'], state);
   }
 
   // destructure the palindromes from the state after execution all the stacks.
-  const { initial_string , ...palindromes } = state;
-  return { ...palindromes }
+  return state.palindromes;
 }
 
-console.log(allPossiblePalindromes('aabcaacdadsaewawt'));
+console.log(allPossiblePalindromes('aabcaacdadsaewawtt'));
 
 // Don't exceed Node js stack size of  11034, to check when string has exceeded it ? use stringslength * stringslength i.e. Max words to compute is 105 in node js. After clean up, the string below is 98 in length
-console.log(allPossiblePalindromes('Dennis, Nell, Edna, Leon, Nedra, Anita, Rolf, Nora, Alice, Carol, Leo, Jane, Reed, Dena, Dale, Basil, Rae, Penny, Lana, Dave, Denny, Lena, Ida'.replace(/\s|,/g, '')));
+console.log(allPossiblePalindromes("DennisNellEdnaLeonNedraAnitaRolfNoraAliceCarolLeoJaneReedDenaDaleBasilRaePennyLanaDaveDennyLenaIda"));
